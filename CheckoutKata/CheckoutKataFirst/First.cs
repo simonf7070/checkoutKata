c405f03a23a7171b7f0c7441c6dc3e8cba500cf6
simon felix <simonf7070@gmail.com>
2015-02-26 08:37:50 +0000

    initial commit

---
+++ CheckoutKata/CheckoutKataFirst/First.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+//A = 50
+//B = 30
+//C = 20
+//D = 15
+
+//3 x A = 130
+//2 x B = 45
+
+//price in euros or pounds
+//best offer ie. ABC = 90
+
+namespace CheckoutKata.CheckoutKataFirst
+{
+    [TestFixture]
+    public class CheckoutTotalShould
+    {
+        [Test]
+        public void ZeroWhenNoItemsAdded()
+        {
+            var checkout = new Checkout();
+            Assert.That(checkout.Total(), Is.EqualTo(0));
+        }
+
+        [TestCase("A", 50)]
+        [TestCase("B", 30)]
+        [TestCase("C", 20)]
+        [TestCase("D", 15)]
+        public void ItemPriceWhenSingleItemScanned(string item, int expectedPrice)
+        {
+            var checkout = new Checkout();
+            checkout.Scan(item);
+            Assert.That(checkout.Total(), Is.EqualTo(expectedPrice));
+        }
+
+        [Test]
+        public void OneHundredWhenTwoAScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void EightyWhenAThenBScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("B");
+            Assert.That(checkout.Total(), Is.EqualTo(80));
+        }
+
+        [Test]
+        public void OneHunderedAndThirtyWhenThreeAScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(130));
+        }
+
+        [Test]
+        public void OneHunderedAndFiftyWhenTwoAThenBCScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            Assert.That(checkout.Total(), Is.EqualTo(150));
+        }
+
+        [Test]
+        public void TwoHunderedAndSixtyWhenSixAScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(260));
+        }
+
+        [Test]
+        public void OneHunderedAndEightyWhenFourAScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(180));
+        }
+
+        [Test]
+        public void FortyFiveWhenTwoBScanned()
+        {
+            var checkout = new Checkout();
+            checkout.Scan("B");
+            checkout.Scan("B");
+            Assert.That(checkout.Total(), Is.EqualTo(45));
+        }
+
+    }
+
+    public class Checkout
+    {
+        private int _subtotal;
+        private List<string> _items;
+
+        public Checkout()
+        {
+            _subtotal = 0;
+            _items = new List<string>();
+        }
+
+        public int Total()
+        {
+            var discountA = ApplyDiscount("A", 3, 20);
+            var discountB = ApplyDiscount("B", 2, 15);
+            return _subtotal - discountA - discountB;
+        }
+
+        private int ApplyDiscount(string item, int quantityRequiredForDiscount, int discountAmount)
+        {
+            var numberOfDiscounts = _items.Count(i => i == item) / quantityRequiredForDiscount;
+            return numberOfDiscounts * discountAmount;
+        }
+
+        public void Scan(string item)
+        {
+            switch (item)
+            {
+                case "A":
+                    AddToTotal(50);
+                    break;
+                case "B":
+                    AddToTotal(30);
+                    break;
+                case "C":
+                    AddToTotal(20);
+                    break;
+                case "D":
+                    AddToTotal(15);
+                    break;
+            }
+            _items.Add(item);
+        }
+
+        private void AddToTotal(int value)
+        {
+            _subtotal += value;
+        }
+    }
+}
---
+++ CheckoutKata/CheckoutKataSecond/Second.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CheckoutKata.CheckoutKataSecond
+{
+    [TestFixture]
+    public class CheckoutTest
+    {
+        [Test]
+        public void SingleItemATotalForItem()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void SingleItemBTotalForItem()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("B");
+            Assert.That(checkout.Total(), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void TwoItemATotalForItems()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ItemAandItemBTotalForItems()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("A");
+            checkout.Scan("B");
+            Assert.That(checkout.Total(), Is.EqualTo(80));
+        }
+
+        [Test]
+        public void AddThreeAshouldTotal130()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(130));
+        }
+
+        [Test]
+        public void AddFourAshouldTotal180()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            Assert.That(checkout.Total(), Is.EqualTo(180));
+        }
+
+        [Test]
+        public void AddTwoBshouldTotal45()
+        {
+            var checkout = CreateCheckout();
+            checkout.Scan("B");
+            checkout.Scan("B");
+            Assert.That(checkout.Total(), Is.EqualTo(45));
+        }
+
+        private static Checkout CreateCheckout()
+        {
+            return new Checkout(new ItemPrice("A", 50), new ItemPrice("B", 30));
+        }
+    }
+
+    public class PriceList
+    {
+        private readonly List<ItemPrice> _inner;
+
+        public PriceList(IEnumerable<ItemPrice> itemPrices)
+        {
+            _inner = itemPrices.ToList();
+        }
+        
+        public int GetPrice(string item)
+        {
+            return _inner.First(i => i.Item == item).Price;
+        }
+    }
+
+    public class ItemPrice
+    {
+        public string Item { get; private set; }
+        public int Price { get; private set; }
+
+        public ItemPrice(string item, int price)
+        {
+            Item = item;
+            Price = price;
+        }
+    }
+
+    public class Checkout
+    {
+        private int _subTotal;
+        private readonly PriceList _priceList;
+        private readonly List<string> _itemsScannedList;
+
+        public Checkout(params ItemPrice[] itemPrice)
+        {
+            _priceList = new PriceList(itemPrice);
+            _itemsScannedList = new List<string>();
+        }
+
+        public int Total()
+        {
+            var discountForA = GetDiscount("A", new Discount(3, 20));
+            var discountForB = GetDiscount("B", new Discount(2, 15));
+            return _subTotal - discountForA - discountForB;
+        }
+
+        private int GetDiscount(string item, Discount discount)
+        {
+            var itemCount = _itemsScannedList.Count(i => i == item);
+            int discountsToApply = itemCount / discount.DiscountQuantity;
+            return discountsToApply * discount.DiscountAmount;
+        }
+
+        public void Scan(string item)
+        {
+            _itemsScannedList.Add(item);
+            _subTotal += _priceList.GetPrice(item);
+        }
+    }
+
+    internal class Discount
+    {
+        public int DiscountQuantity { get; private set; }
+        public int DiscountAmount { get; private set; }
+
+        public Discount(int discountQuantity, int discountAmount)
+        {
+            DiscountQuantity = discountQuantity;
+            DiscountAmount = discountAmount;
+        }
+    }
+}
